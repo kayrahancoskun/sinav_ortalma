f93f692947e042530ff81a98ea89aa1f084efdef
kayrahancoskun <kayrahancoskun10@gmail.com>
2024-06-27 16:55:48 +0300

    Add files via upload

---
+++ ortalama.py
@@ -0,0 +1,7 @@
+def sinav_ortalamasi():
+    not1=int(input("ilk notu giriniz"))
+    not2=int(input("ikinci notu giriniz"))
+    not3=int(input("ikinci notu giriniz"))
+    ortalama=(not1+not2+not3)/3
+    print(ortalama)
+sinav_ortalamasi()
